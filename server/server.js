0b2fa679b1360bde66e5f62fb6744f67c9e39622
smitb7 <smitbhavsar7@icloud.com>
2026-03-05 17:25:31 +0530

    server check

---
+++ server/server.js
@@ -0,0 +1,15 @@
+
+const express = require("express")
+const app = express();
+
+
+
+
+
+app.listen(8080, (err)=>{
+    
+    console.log("Your server is running on Localhost:8080");
+    
+    
+})
+
