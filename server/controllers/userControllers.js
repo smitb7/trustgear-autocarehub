c6327e1ba2b6507d43119f8feb65a098a03215bd
smitb7 <smitbhavsar7@icloud.com>
2026-03-11 10:40:10 +0530

    API users update by ID is working perfect

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -3,6 +3,7 @@ const User = require("../models/usersSchema")
 
 
 
+
 // so now we will get the all users first 
 const getUsers = async(req,res)=>{
 
@@ -29,6 +30,38 @@ const getUsers = async(req,res)=>{
 
 }
 
+
+// now we will make a findbyID so that we can find the user by there ID 
+
+
+const getUsersbyId = async (req,res)=>{
+
+   
+    try {
+
+        const {
+            id
+        } = req.params 
+        
+        const findDatabyId = await User.findById(id)
+
+        res.status(200).json({
+            data : findDatabyId,
+            message : "Here's your data according your ID..!"
+        })
+    
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(" Something is wrong in your server...!")
+                
+    }
+
+
+}
+
+
+
+
 // now we will Update a users by ID  
 
 const updateUser = async(req,res)=>{
@@ -43,6 +76,12 @@ const updateUser = async(req,res)=>{
 
         const updateUserdata =  await User.findByIdAndUpdate(
             id,
+            {
+                name:name,
+                email : email,
+                password : password,
+                role : role
+            },
             {new:true} // for confirmation only
         )
 
@@ -68,6 +107,15 @@ const updateUser = async(req,res)=>{
 
 
 
+//  Delete By ID 
 
-module.exports = {getUsers, updateUser}
+const deleteUserbyId = async (req,res)=>{
+
+
+}
+
+
+
+
+module.exports = {getUsers, updateUser, getUsersbyId}
 
--- server/routs/userRoutes.js
+++ server/routs/userRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express')
 
 
-const {getUsers, updateUser} = require("../controllers/userControllers")
+const {getUsers, updateUser, getUsersbyId} = require("../controllers/userControllers")
 // define users router 
 const usersRouter = express.Router()
 
 
 usersRouter.get("/", getUsers)
+usersRouter.get("/:id", getUsersbyId)
 usersRouter.put("/:id", updateUser)
+
 
 module.exports = {usersRouter}
