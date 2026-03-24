b7c47d83064a6596c6db3721f3669d5b8c2bd799
smitb7 <smitbhavsar7@icloud.com>
2026-03-24 14:25:32 +0530

    token err handle

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,58 +1,49 @@
-const express = require("express")
 const jwt = require("jsonwebtoken")
 const BlacklistToken = require("../models/blackListTokenschema");
 
 
 
-
-
-
-const authMiddleware = async (req,res,next)=>{
+const authMiddleware = async(req,res,next)=>{
 
     try {
         
-        const authHeader = req.headers["authorization"]
-                // console.log(token);
+        const token = req.headers["authorization"]
 
-        const token = authHeader.split(" ")[1];
+        if(!token){
+            res.status(401).json({
 
-                if(!token){
-                    res.status(401).send("Please login first...!")
-                }
+                message : "Please Login first"
                 
-                const tokenExist = await BlacklistToken.findOne({
-                    token
-                })
+            })
+        }
 
-                if(tokenExist){
-                    res.status(403).json({
-                        message : " You are logged out . please generate a new token"
-                    })
-                }
+        // check blacklist
+        const tokenExist = await BlacklistToken.findOne({token})
 
-                const decoded = jwt.verify(token, "This-is-super-secret-string-which-can-be-anything", (err,decoded)=>{
-                    if(err){
-                        res.status(401).send("Your Token is not valid..!")
-                    }
-                    // console.log (decoded);
-                    req.user = decoded;
-                    next() ; 
-                })
+        if(tokenExist){
+            res.status(403).json({
+                message : "Token expired, login again..!"
+            })
+        }
+
+        // verify token 
+        const decoded = jwt.verify(
+            token,
+            "This-is-super-secret-string-which-can-be-anything"
+        )
+
+        req.user = decoded;
+        next() // you permitted 
 
 
-    } catch (error) {
-
-        console.log(error);
-        res.status(404).json({
-
-            message : "Something went wrong"
-
-        })
+    } catch (err) {
         
-    }
+        res.status(401).json({
+            message : "invalid or expired token"
+        })
 
+    }
 
 }
 
-
-module.exports = authMiddleware ;
+module.exports = authMiddleware; 
